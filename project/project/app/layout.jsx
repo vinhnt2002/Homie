abb245fdd6cdc98aaffe420a5e8f971709103f6d
vinh nguyễn <vinhntse2002@gmail.com>
2023-05-15 22:46:04 +0700

    build

---
+++ project/project/app/layout.jsx
@@ -0,0 +1,34 @@
+
+// import Nav from '@components/Nav'
+import Footer from '@components/Footer'
+import Header from '@components/Header'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata = {
+  title: "Hommie Shop",
+  description: 'Hommie Hommie'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
+
+          <main className='app'>
+            <Header/>
+            <Nav />
+            {children}
+            <Footer/>
+          </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
