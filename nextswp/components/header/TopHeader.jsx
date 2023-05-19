c3914de2e1425a57bbbfc43558476bf84c76aac1
Chi Trung <trunghcw@gmail.com>
2023-05-19 23:14:39 +0700

    Header

---
+++ nextswp/components/header/TopHeader.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const TopHeader = () => {
+  return (
+    <div className="top-header d-none d-md-block">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-8"></div>
+            <div className="col-lg-6 col-md-4">
+              <div className="d-flex align-items-center justify-content-center justify-content-md-end">
+                <div className="lng_dropdown">
+                  <select name="countries" className="custome_select">
+                    <option
+                      value="en"
+                      data-image="assets/images/eng.png"
+                      data-title="English"
+                    >
+                      English
+                    </option>
+                    <option
+                      value="fn"
+                      data-image="assets/images/fn.png"
+                      data-title="France"
+                    >
+                      France
+                    </option>
+                    <option
+                      value="us"
+                      data-image="assets/images/us.png"
+                      data-title="United States"
+                    >
+                      United States
+                    </option>
+                  </select>
+                </div>
+                <div className="ms-3">
+                  <select name="countries" className="custome_select">
+                    <option value="USD" data-title="USD">
+                      USD
+                    </option>
+                    <option value="EUR" data-title="EUR">
+                      EUR
+                    </option>
+                    <option value="GBR" data-title="GBR">
+                      GBR
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default TopHeader
