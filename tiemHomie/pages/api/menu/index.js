000ead4a836227c5082477898291d044dcd118c3
Minh <minhkieu702@gmail.com>
2023-07-05 03:22:48 +0700

    test

--- tiemHomie/pages/api/menu/index.js
+++ tiemHomie/pages/api/menu/index.js
@@ -1,12 +1,15 @@
 import Menu from "../../../models/menu";
 import { connectToDB } from "../../../utils/database";
+import { getAllProduct } from "../../../action/menuApi";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       await connectToDB();
+      const data = await getAllProduct();
+      await Menu.deleteMany({});
+      await Menu.insertMany(Array.isArray(data) ? data : [data]);
       const menu = await Menu.find({});
-      
       res.status(200).json(menu);
     } catch (error) {
       console.log(error);
