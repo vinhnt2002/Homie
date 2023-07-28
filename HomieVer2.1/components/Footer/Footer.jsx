99c6e6a6bdeea4cf4f6a4083765c9d53210877e4
vinhnt2002 <124482049+vinhnt2002@users.noreply.github.com>
2023-07-28 12:08:29 +0700

    Update Footer.jsx

--- HomieVer2.1/components/Footer/Footer.jsx
+++ HomieVer2.1/components/Footer/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = ({}) => {
                             <div className="icon_box_content"> 
                             <h5>Miễn Phí vận chuyển</h5>
                             <p >
-                              Miễn phí vận chuyển toàn quốc cho đơn hàng từ 600k.<br></br> Ship nhanh trong vòng 2h các quận nội thành HCM, phí ship chỉ từ 15-30k.
+                              Miễn phí vận chuyển toàn quốc cho đơn hàng từ 600k. Ship nhanh trong vòng 2h
                             </p>
                           </div>
                         </div>
